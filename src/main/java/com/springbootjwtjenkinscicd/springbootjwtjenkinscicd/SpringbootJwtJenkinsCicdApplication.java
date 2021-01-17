b744ec2f4f100be0cfa399e5b857d2f81feeaa29
sanjay <sanjay@localhost>
2021-01-17 20:58:17 +0530

    initial commit

---
+++ src/main/java/com/springbootjwtjenkinscicd/springbootjwtjenkinscicd/SpringbootJwtJenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjwtjenkinscicd.springbootjwtjenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtJenkinsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtJenkinsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootjwtjenkinscicd/springbootjwtjenkinscicd/SpringbootJwtJenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjwtjenkinscicd.springbootjwtjenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtJenkinsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
