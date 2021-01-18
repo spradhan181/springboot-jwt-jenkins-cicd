6c2126a532c56d0cd7ad281fa3d138f1e6e7beab
sanjay <sanjay@localhost>
2021-01-18 15:18:20 +0530

    commit

--- src/main/java/com/springboot/jwt/jenkins/cicd/resource/CICDApplication.java
+++ src/main/java/com/springboot/jwt/jenkins/cicd/resource/CICDApplication.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CiICDApplication {
-	
+public class CICDApplication {
+
+	/**
+	 * This method tests the get URL
+	 * 
+	 * @return
+	 */
 	@GetMapping(path = "/test")
 	public String test() {
 		return "SUCCESS";
